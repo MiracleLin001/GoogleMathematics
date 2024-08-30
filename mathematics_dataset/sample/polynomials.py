3556d1f176f75d1f8a3a66b1c65409429be9a6b3
Pengxiao Lin <71924394+MiracleLin001@users.noreply.github.com>
2024-08-30 20:45:18 +0800

    Update polynomials.py
    rename np.object to object.

--- mathematics_dataset/sample/polynomials.py
+++ mathematics_dataset/sample/polynomials.py
@@ -238,7 +238,7 @@ def expand_coefficients(coefficients, entropy, length=None):
   coefficients = np.asarray(coefficients)
   shape = coefficients.shape
 
-  expanded_coefficients = np.empty(shape, dtype=np.object)
+  expanded_coefficients = np.empty(shape, dtype=object)
 
   min_length = np.count_nonzero(coefficients) + 2
   if length is None:
@@ -363,8 +363,8 @@ def coefficients_linear_split(coefficients, entropy):
   if random.choice([False, True]):
     a, b = b, a
 
-  coefficients_1 = np.zeros(coefficients.shape, dtype=np.object)
-  coefficients_2 = np.zeros(coefficients.shape, dtype=np.object)
+  coefficients_1 = np.zeros(coefficients.shape, dtype=object)
+  coefficients_2 = np.zeros(coefficients.shape, dtype=object)
 
   for index, coefficient in enumerate(coefficients):
     entropy_coeff = entropy_coefficients[index]
